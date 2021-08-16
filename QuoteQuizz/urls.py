fa458a368902264da5efff27661cd61601b6b6df
Helen Zapal <helenzapal@mail.ru>
2021-08-16 15:36:19 +0300

    Add root url

--- QuoteQuizz/urls.py
+++ QuoteQuizz/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('quiz.urls')),
     path('admin/', admin.site.urls),
     path('quiz/', include('quiz.urls')),
 ]
